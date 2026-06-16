3102cbb21bacf57c63a0e5d6849910e05bd39059
pranjay19 <pranjayguleria@gmail.com>
2026-06-16 22:19:32 +0530

    solved Question 12: Final Account Balance

--- sql_practice_datalemur.sql
+++ sql_practice_datalemur.sql
@@ -242,3 +242,15 @@ table_1
 --- Question 12: Final Account Balance
 
 --- solution: 
+
+SELECT
+    account_id,
+    SUM(
+        CASE
+            WHEN transaction_type = 'Deposit' THEN amount
+            ELSE -amount
+        END
+    ) AS final_balance
+FROM transactions
+GROUP BY account_id;
+
